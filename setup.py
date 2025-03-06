52fa8761aada5f788941c9a0078e46b5f8566347
Hélio Santos <heliosantos99@gmail.com>
2025-03-06 23:06:32 +0000

    update docs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aws_session_manager',
-    version='1.0',
+    version='1.1',
     description='A command line UI to manage AWS Session Manager sessions',
     packages=find_packages(),
 )
